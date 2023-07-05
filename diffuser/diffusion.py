12a0bbdec8e0f0e11be2701015f39a1dbe831111
junwoo <junwoochang@yonsei.ac.kr>
2023-07-05 18:07:16 +0900

    diffuser

---
+++ diffuser/diffusion.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+from torch import nn
+import pdb
+
+import utils
+from helpers import(
+    cosine_beta_schedule,
+    extract,
+    apply_conditioning,
+    Losses,    
+)
+
+class GaussianDiffusion(nn.Modlue):
+    def __init__(self, model, horizon, observation_dim, action_dim, n_timesteps=1000,
+        loss_type='l1', clip_denoised=False, predict_epsilon=True,
+        action_weight=1.0, loss_discount=1.0, loss_weights=None,
+    ):
+        super().__init__()
+        self.horizon = horizon
+        self.observation_dim = observation_dim
+        self.action_dim = action_dim
+        self.transition_dim = observation_dim + action_dim
+        self.model = model
+        
+        betas = cosine_beta_schedule(n_timesteps)
+        alphas = 1. - betas
+        alphas_cumprod = torch.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = torch.cat([torch.ones(1), alphas_cumprod[:-1]])
+        
+        self.n_timesteps = int(n_timesteps)
+        self.clip_denoised = clip_denoised
+        self.predict_epsilon = predict_epsilon
+        
+        self.register_buffer('betas', betas)
+        self.register_buffer('alphas_cumprod', alphas_cumprod)
+        self.register_buffer('alphas_cumprod_prev', alphas_cumprod_prev)
+            
+        
